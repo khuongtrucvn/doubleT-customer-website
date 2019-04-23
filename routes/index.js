1a71a37793af1a5d0b95e1ff58e22632bd05b72b
khuongtrucvn <supertruc2102@yahoo.com>
2019-04-23 17:15:58 +0700

    Add forgot password layout and fix bugs

--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,10 @@ router.get('/dang-nhap-dang-ki.hbs', function(req, res, next) {
   res.render('dang-nhap-dang-ki', { title: 'DoubleT | Đăng nhập - Đăng kí' });
 });
 
+router.get('/quen-mat-khau.hbs', function(req, res, next) {
+  res.render('quen-mat-khau', { title: 'DoubleT | Quên mật khẩu' });
+});
+
 router.get('/tai-khoan.hbs', function(req, res, next) {
   res.render('tai-khoan', { title: 'DoubleT | Tài khoản' });
 });
