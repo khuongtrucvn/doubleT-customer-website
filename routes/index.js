b1668eee83bcddfd3882577567ef70b708af9d75
khuongtrucvn <supertruc2102@yahoo.com>
2019-04-23 16:42:21 +0700

    Add login and register layout

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('trang-chu', { title: 'DoubleT | Trang chủ' });
 });
 
+router.get('/tai-khoan.hbs', function(req, res, next) {
+  res.render('tai-khoan', { title: 'DoubleT | Tài khoản' });
+});
+
 module.exports = router;
