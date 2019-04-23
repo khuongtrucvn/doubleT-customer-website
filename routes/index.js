26bf80ffb8c9c9f3658c7df0972958fce82f8a83
khuongtrucvn <supertruc2102@yahoo.com>
2019-04-23 16:57:03 +0700

    Add account layout

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('trang-chu', { title: 'DoubleT | Trang chủ' });
 });
 
+router.get('/dang-nhap-dang-ki.hbs', function(req, res, next) {
+  res.render('dang-nhap-dang-ki', { title: 'DoubleT | Đăng nhập - Đăng kí' });
+});
+
 router.get('/tai-khoan.hbs', function(req, res, next) {
   res.render('tai-khoan', { title: 'DoubleT | Tài khoản' });
 });
